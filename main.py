f78dd634398f0b23f656db1c6634a26f0cc27c2a
Niki <39004156+iByNiki@users.noreply.github.com>
2023-07-08 16:17:57 +0200

    .

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 
 HOST = "0.0.0.0"
 PORT = int(sys.argv[1])
+WEBDIR = sys.argv[2]
 
 class ClientThread(threading.Thread):
     def __init__(self, ip, port, socket):
@@ -17,9 +18,19 @@ class ClientThread(threading.Thread):
         lines = request.split("\n")
         
         dir = lines[0].split(" ")[1]
-        print(dir)
+        if (dir[-1] == "/"):
+            dir += "index.html"
 
-        self.socket.send("HTTP/1.1 200 OK\n\nHELLO!".encode())
+        try:
+            f = open(WEBDIR + dir, "r")
+            data = f.read()
+            f.close()
+        except FileNotFoundError:
+            self.socket.send("HTTP/1.1 404 Not Found\n\nFile not found.".encode())
+            self.socket.close()
+            return
+
+        self.socket.send(("HTTP/1.1 200 OK\n\n" + data).encode())
         self.socket.close()
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
