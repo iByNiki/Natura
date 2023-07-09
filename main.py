481d52a870df7dd79965c21166c34f5525c9e45f
Niki <39004156+iByNiki@users.noreply.github.com>
2023-07-09 16:02:49 +0200

    .

--- main.py
+++ main.py
@@ -6,15 +6,26 @@ from tcpserver import TCPServer
 from cache import Cache
 from settings import Settings
 
+class ServerThread(threading.Thread):
+    def __init__(self, settings, cache, type):
+        threading.Thread.__init__(self)
+        self.tcpserver = TCPServer(settings, cache)
+    def run(self):
+        self.tcpserver.start()
+        self.tcpserver.loop()
+
 if (__name__ == "__main__"):
     settings = Settings()
     settings.load()
-    # settings.loadFromFile()
+
     cache = Cache(settings)
 
-    tcpserver = TCPServer(settings, cache)
-    tcpserver.start()
-    tcpserver.loop()
+    httpThread = ServerThread(settings, cache, "http")
+    #sslThread = ServerThread(settings, cache, "https")
+
+    httpThread.start()
+    #sslThread.start()
+    
 
 # TODO: ADD SUPPORT FOR FILE SENDING WTF
 # TODO: Idea - add a plugin system to intercept requests etc
